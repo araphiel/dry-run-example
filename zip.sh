d603d22abed7b740dd1bf60336ed0ebcab36afa6
Armon Raphiel <araphiel@gmail.com>
2024-06-14 12:42:05 -0500

    chore: update

--- zip.sh
+++ zip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -type d -name 'dist' -path '*/packages/*' | while read -r dir; do
-  parent_dir=$(basename "$(dirname "$dir")")
-  zip -r "${dir%/*}/${parent_dir}.zip" "$dir"
+find ./packages -type d -name 'dist' | while IFS= read -r dist_dir; do
+  parent_dir=$(basename "$(dirname "$dist_dir")")
+  (cd "$dist_dir" && zip -r "../${parent_dir}.zip" .)
 done
